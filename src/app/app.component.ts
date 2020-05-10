6976c14994eb3caef9ca862c30c5ee1329700ea5
shivangimittal791 <shivangimittal791@gmail.com>
2020-05-10 16:12:40 +0530

    form-page

---
+++ src/app/app.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'
+  ,'./vendor/bootstrap/css/bootstrap.min.css'
+,'./css/styles.css']
+})
+export class AppComponent {
+  title = 'form-page';
+}
